44349b5c1c5c6c718fd2ca768374514dcf0c7fb2
JackT_ <jacktravers140@gmail.com>
2024-09-19 14:37:25 +0100

    finished project

---
+++ script.js
@@ -0,0 +1,66 @@
+// const currentDate = new Date();
+// const yyyy = currentDate.getFullYear();
+// const mm = currentDate.getMonth() + 1; // This is because months start at 0, put this here because i wont remember
+// const dd = currentDate.getDate();
+// console.log(currentDate);
+// console.log(`The Date is: ${dd}/${mm}/${yyyy}`);
+
+// let target_date = new Date("2024-09-22");
+
+// let time_difference = target_date - currentDate;
+
+// let dayDifference = time_difference / (1000 * 60 * 60 * 24); // 1000ms in a second, 60 seconds in a min, 60 mins in an hour, 24 hours in a day
+// console.log(Math.floor(dayDifference));
+
+// // Calculations and stuff i found online to help make this
+// let remainingTime = time_difference;
+
+// const days = Math.floor(time_difference / (1000 * 60 * 60 * 24));
+// time_difference -= days * 1000 * 60 * 60 * 24;
+
+// const hours = Math.floor(time_difference / (1000 * 60 * 60));
+// time_difference -= hours * 1000 * 60 * 60;
+
+// const minutes = Math.floor(time_difference / (1000 * 60));
+// time_difference -= minutes * 1000 * 60;
+
+// const seconds = Math.floor(time_difference / 1000);
+
+// console.log(`Days: ${days}\nSeconds: ${seconds}\nMinutes: ${minutes}\nHours: ${hours}`);
+
+
+
+function updateCountdown(){
+    const currentDate = new Date();
+    const yyyy = currentDate.getFullYear();
+    const mm = currentDate.getMonth() + 1; // This is because months start at 0, put this here because i wont remember
+    const dd = currentDate.getDate();
+    console.log(currentDate);
+    console.log(`The Date is: ${dd}/${mm}/${yyyy}`);
+
+    let target_date = new Date("2024-09-22");
+
+    let time_difference = target_date - currentDate;
+
+    let dayDifference = time_difference / (1000 * 60 * 60 * 24); // 1000ms in a second, 60 seconds in a min, 60 mins in an hour, 24 hours in a day
+    console.log(Math.floor(dayDifference));
+
+    // Calculations and stuff i found online to help make this
+    let remainingTime = time_difference;
+
+    const days = Math.floor(time_difference / (1000 * 60 * 60 * 24));
+    time_difference -= days * 1000 * 60 * 60 * 24;
+
+    const hours = Math.floor(time_difference / (1000 * 60 * 60));
+    time_difference -= hours * 1000 * 60 * 60;
+
+    const minutes = Math.floor(time_difference / (1000 * 60));
+    time_difference -= minutes * 1000 * 60;
+
+    const seconds = Math.floor(time_difference / 1000);
+
+    console.log(`Days: ${days}\nSeconds: ${seconds}\nMinutes: ${minutes}\nHours: ${hours}`);
+
+}
+
+setInterval(updateCountdown, 1000);
